e003ce79d0b802209400bd391abc970d288af785
Yaeli778 <yaeli778@gmail.com>
2025-05-10 23:21:00 +0300

    Delete project directory

--- project/content.js
+++
@@ -1,8 +0,0 @@
-// This script runs in the context of the web page
-// For this simple extension, we don't need complex content script functionality
-
-// Send the URL to the background script for analysis
-chrome.runtime.sendMessage({
-  action: "analyzeURL",
-  url: window.location.href
-});
